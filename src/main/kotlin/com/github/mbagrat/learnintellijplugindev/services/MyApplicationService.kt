97de885c3a1f4a1773eeb025b2dad37fc5267b88
GitHub Action <action@github.com>
2021-03-07 12:42:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mbagrat/learnintellijplugindev/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mbagrat.learnintellijplugindev.services
+
+import com.github.mbagrat.learnintellijplugindev.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mbagrat/learnintellijplugindev/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mbagrat/learnintellijplugindev/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mbagrat.learnintellijplugindev.services
 
+import com.github.mbagrat.learnintellijplugindev.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
